1ac40ee62e06f7675097280fcb5cdb797771673c
bulutluoz <abbulutluoz@gmail.com>
2025-02-06 09:38:50 +0100

    3.ders sonu

--- Day02_Q04_TablodaDetayliSoruYapma.sql
+++ Day02_Q04_TablodaDetayliSoruYapma.sql
@@ -63,6 +63,39 @@ WHERE kategori = "MACERA";
 -- kitaplar tablosunda Lev Tolstoy'a ait kitaplarin
 -- ismini, yayin tarihini ve kategorisini listeleyin
 
+SELECT kitap_adi,yayin_yili,kategori FROM kitaplar
+WHERE yazar_adi = 'Lev Tolstoy';
+
+
+CREATE TABLE people
+(
+id INT,
+isim VARCHAR(30),
+soyisim VARCHAR(30),
+yas INT,
+sehir VARCHAR(20)
+);
+
+INSERT INTO people () VALUES (1,'Ahmet', 'Yılmaz', 30, 'Ankara');
+INSERT INTO people () VALUES (2,'Mehmet', 'Kara', 25, 'İstanbul');
+INSERT INTO people () VALUES (3,'Ayşe', 'Kaya', 22, 'İzmir');
+INSERT INTO people () VALUES (4,'Fatma', 'Güneş', 28, 'Bursa');
+INSERT INTO people () VALUES (5,'Ali', 'Çelik', 35, 'Antalya');
+INSERT INTO people () VALUES (6,'Elif', 'Demir', 29, 'Ankara');
+INSERT INTO people () VALUES (7,'Can', 'Özdemir', 33, 'İstanbul');
+INSERT INTO people () VALUES (8,'Deniz', 'Arslan', 24, 'İzmir');
+INSERT INTO people () VALUES (9,'Selin', 'Yıldız', 27, 'Bursa');
+INSERT INTO people () VALUES (10,'Emre', 'Koç', 31, 'Antalya');
+INSERT INTO people () VALUES (11,'Zeynep', 'Çınar', 26, 'Konya');
+INSERT INTO people () VALUES (12,'Oğuz', 'Kaplan', 40, 'Samsun');
+INSERT INTO people () VALUES (13,'Burcu', 'Ertuğrul', 22, 'Trabzon');
+INSERT INTO people () VALUES (14,'Kaya', 'Durmaz', 35, 'Diyarbakır');
+INSERT INTO people () VALUES (15,'Seda', 'Aydın', 28, 'Eskişehir');
+
+SELECT * FROM people;
+
+
+
 
 
 
