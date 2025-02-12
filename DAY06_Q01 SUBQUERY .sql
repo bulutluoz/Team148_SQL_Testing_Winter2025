10669499057d543709c99f0f244c75979320ad31
bulutluoz <abbulutluoz@gmail.com>
2025-02-12 11:20:31 +0100

    Ders06 sonu

--- DAY06_Q01 SUBQUERY .sql
+++ DAY06_Q01 SUBQUERY .sql
@@ -267,15 +267,83 @@ SELECT sirket, calisanlar_sayisi,
 FROM sirketler;
 
 
+/* 
+======================== EXISTS CONDITION ===========================
+EXISTS Condition subquery'ler ile kullanilir. IN ifadesinin kullanimina
+benzer olarak, EXISTS ve NOT EXISTS ifadeleri de alt sorgudan getirilen 
+degerlerin icerisinde bir degerin olmasi veya olmamasi durumunda islem 
+yapilmasini saglar.
+======================================================================
+*/
+
+CREATE TABLE mayis_satislar
+(
+urun_id int,
+musteri_isim varchar(50),
+urun_isim varchar(50)
+);
+
+INSERT INTO mayis_satislar VALUES (10, 'Mark', 'Honda');
+INSERT INTO mayis_satislar VALUES (10, 'Mark', 'Honda');
+INSERT INTO mayis_satislar VALUES (20, 'John', 'Toyota');
+INSERT INTO mayis_satislar VALUES (30, 'Amy', 'Ford');
+INSERT INTO mayis_satislar VALUES (20, 'Mark', 'Toyota');
+INSERT INTO mayis_satislar VALUES (10, 'Adem', 'Honda');
+INSERT INTO mayis_satislar VALUES (40, 'John', 'Hyundai');
+INSERT INTO mayis_satislar VALUES (20, 'Eddie', 'Toyota');
+
+CREATE TABLE nisan_satislar
+(
+urun_id int,
+musteri_isim varchar(50),
+urun_isim varchar(50)
+);
+
+INSERT INTO nisan_satislar VALUES (10, 'Hasan', 'Honda');
+INSERT INTO nisan_satislar VALUES (10, 'Kemal', 'Honda');
+INSERT INTO nisan_satislar VALUES (20, 'Ayse', 'Toyota');
+INSERT INTO nisan_satislar VALUES (50, 'Yasar', 'Volvo');
+INSERT INTO nisan_satislar VALUES (20, 'Mine', 'Toyota');
+
+SELECT * FROM mayis_satislar;
+SELECT * FROM nisan_satislar;
+
+
+/*
+----------------------------------------------------------------
+SORU 1 : Her iki ayda da ayni id ile satilan urunlerin urun_id'lerini
+ve urunleri mayis ayinda alanlarin isimlerini getiren bir query yaziniz.
+----------------------------------------------------------------
+*/
+
+SELECT urun_id, musteri_isim
+FROM mayis_satislar
+WHERE urun_id  IN ( SELECT urun_id 
+				   FROM nisan_satislar
+                   WHERE nisan_satislar.urun_id = mayis_satislar.urun_id); 
 
 
 
+SELECT urun_id, musteri_isim
+FROM mayis_satislar
+WHERE EXISTS ( SELECT urun_id 
+			   FROM nisan_satislar
+			   WHERE nisan_satislar.urun_id = mayis_satislar.urun_id); 
 
 
+-- mayis ayinda olup, nisan ayinda olmayan id'leri goruntulemek icin
+SELECT urun_id, musteri_isim
+FROM mayis_satislar
+WHERE urun_id  NOT IN ( SELECT urun_id 
+				        FROM nisan_satislar
+                        WHERE nisan_satislar.urun_id = mayis_satislar.urun_id); 
 
 
-
-
+SELECT urun_id, musteri_isim
+FROM mayis_satislar
+WHERE NOT EXISTS ( SELECT urun_id 
+			       FROM nisan_satislar
+			       WHERE nisan_satislar.urun_id = mayis_satislar.urun_id); 
 
 
 
