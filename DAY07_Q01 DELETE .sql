3e1c8fcccc71acf73ef761fff1976c1fdf702d3e
bulutluoz <abbulutluoz@gmail.com>
2025-02-13 10:12:55 +0100

    Ders07 sonu

--- DAY07_Q01 DELETE .sql
+++ DAY07_Q01 DELETE .sql
@@ -54,6 +54,131 @@ SAVEPOINT sabah;
 
 -- isminde a harfi olan ogrencilerin yazili notunu 5 artirin
 
+UPDATE kursiyer
+SET yazili_notu = yazili_notu + 5
+WHERE isim LIKE '%a%';
+
+-- veli ismi 3 harften fazla olanlardan 3 puan silin
+
+UPDATE kursiyer
+SET yazili_notu = yazili_notu - 3
+WHERE veli_isim LIKE '___%';
+
+-- veli ismi Hasan olan ogrenciyi silin
+
+DELETE FROM kursiyer
+WHERE veli_isim = 'Hasan' ;
+
+ROLLBACK TO sabah;
+
+
+-- SAVEPOINT calistirildigi anda ki guncel durumu dondurur ve o haliyle yedekler.
+-- Tekrar ROLLBACK TO komutu ile cagirildiginda ise dondurdugu verileri getirir.
+-- Yani SAVEPOINT isleminden ROLLBACK islemine kadar yapilan hic bir islem 
+-- SAVEPOINT ile kaydedilmeyecegi icin 
+-- ROLLBACK TO komutu ile geri getirilemez.
+
+
+/*
+	TRUNCATE komutu kayitlari geri getirilemeyecek sekilde siler
+*/
+
+
+-- kursiyer tablosundaki verileri TRUNCATE ile silin
+TRUNCATE kursiyer;
+
+ROLLBACK TO sabah; -- does not exist
+
+
+
+CREATE TABLE talebeler
+	(
+		id CHAR(3) PRIMARY KEY,
+		isim VARCHAR(50),
+		veli_isim VARCHAR(50),
+		yazili_notu INT
+	);
+ 
+ INSERT INTO talebeler VALUES
+		(123,'Ali Can', 'Hasan', 75),
+		(124,'Merve Gul', 'Ayse', 85),
+		(125,'Kemal Yasa', 'Hasan', 85),
+		(126,'Nesıbe Yılmaz', 'Ayse', 95),
+		(127,'Mustafa Bak', 'Can', 99);
+
+
+CREATE TABLE dereceler
+		(
+			talebe_id CHAR(3),
+			ders_adi VARCHAR(30),
+			yazili_notu INT,
+			CONSTRAINT talebe_id_fk FOREIGN KEY (talebe_id)
+			REFERENCES talebeler(id) 
+		);
+
+INSERT INTO dereceler VALUES
+		(123, 'Kimya', 75),
+		(124, 'Fizik', 65),
+		(125, 'Tarih', 90),
+		(126, 'Matematik', 90),
+		(127, 'Matematik', 23);
+
+
+
+SELECT * FROM talebeler;
+SELECT * FROM dereceler;
+
+
+-- Talebeler tablosundan id'si 125 ten buyuk olan datalari siliniz. 
+
+DELETE FROM talebeler
+WHERE id > 125;
+
+-- Talebeler tablosu parent tablo oldugundan
+-- talebeler tablosundan silmeden once dereceler tablosundan 
+-- bu kayitlari silmelisiniz
+
+DELETE FROM dereceler
+WHERE talebe_id > 125;
+
+DELETE FROM talebeler
+WHERE id > 125;
+
+-- -------------------ON DELETE CASCADE-------------
+
+/*
+        Her defasinda once child tablodaki verileri silmek yerine 
+		ON DELETE CASCADE silme ozelligini aktif hale getirebiliriz.
+
+		Bunun icin FK olan satirin en sonuna ON DELETE CASCADE 
+        komutunu yazmak yeterli.
+*/
+
+DROP TABLE dereceler;
+TRUNCATE talebeler;
+
+-- dereceler tablosunu yeniden olusturalim 
+
+CREATE TABLE dereceler
+		(
+			talebe_id CHAR(3),
+			ders_adi VARCHAR(30),
+			yazili_notu INT,
+			CONSTRAINT talebe_id_fk FOREIGN KEY (talebe_id)
+			REFERENCES talebeler(id)  ON DELETE CASCADE
+		);
+
+
+-- Talebeler tablosundan id'si 125 ten buyuk olan datalari siliniz. 
+DELETE FROM talebeler
+WHERE id > 125;
+
+
+
+
+
+
+
 
 
 
